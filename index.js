6b0de3a0cf76ccbcf17de924a8e2b6845f7a95c7
samiramelo <samiramelo54@gmail.com>
2023-02-22 21:45:17 -0300

    Corregido

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ app.use(express.urlencoded({ extended: false }))
 
 //Configurar Express para permitir conexión a una dirección en particular.
 //Para permitir a cualquier dirección, app.use(cors())
-app.use(cors({
-    origin: ['http://localhost:5173']
-  }));
   
+  app.use(cors({
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173']
+  }));
+
 app.use(routes)
 
 app.listen(process.env.PORT, () => {
